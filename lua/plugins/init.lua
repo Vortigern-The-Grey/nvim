981e390f0f213b47b25fe26856c0a607b22217b0
Vortigern-The-Grey <matthewkenyon2008@outlook.com>
2024-08-16 07:03:59 +1200

    Add todo-comments.nvim to plugins/init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -107,4 +107,13 @@ return {
       vim.notify = require "notify"
     end,
   },
+  { 
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  }, 
 }
