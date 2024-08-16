c5cc3a835dd6132222437e81061c5628c454d874
Vortigern-The-Grey <matthewkenyon2008@outlook.com>
2024-08-17 08:54:28 +1200

    remove silly spaces uwu

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -107,13 +107,14 @@ return {
       vim.notify = require "notify"
     end,
   },
-  { 
+  {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     },
-  }, 
+  },
 }
