2f1a553ab6bae39794fd17240d1db1e768175854
Matthew Kenyon <matthewkenyon2008@outlook.com>
2025-07-28 12:26:14 +1200

    Add store.nvim and dependencies

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -156,4 +156,17 @@ return {
       })
     end,
   },
+  {
+    "alex-popov-tech/store.nvim",
+    dependencies = {
+      "OXY2DEV/markview.nvim", -- optional, for pretty readme preview / help window
+    },
+    cmd = "Store",
+    keys = {
+      { "<leader>s", "<cmd>Store<cr>", desc = "Open Plugin Store" },
+    },
+    opts = {
+    -- optional configuration here
+    },
+  },
 }
