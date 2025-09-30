55ef3d260dff0b8690e5e0d8832f3d2488158e31
Matthew Kenyon <matthewkenyon2008@outlook.com>
2025-10-01 08:34:32 +1300

    add feed.nvim keybind

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,27 +8,28 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("n", "<leader>lz", "<cmd> :Lazy <CR>")
+map("n", "<leader>rss", "<cmd> :Feed <CR>")
 map("n", "<leader>lg", "<cmd> :LazyGit <CR>")
 map("n", "<leader>fl", "<cmd> :FloatermNew --height=0.75 --width=0.75 <CR>")
 map("n", "<leader>tr", "<cmd> :Trouble <CR>")
 map("n", "<leader>ms", "<cmd> :Mason <CR>")
 map("n", "<leader>pr", function()
-    require("pretty_hover").hover()
+  require("pretty_hover").hover()
 end)
 map("n", "<leader>ma", "<cmd> :MasonInstallAll <CR>")
-if package.config:sub(1, 1) == '\\' then
-    map("n", "<leader>cf", "<cmd> :cd $LOCALAPPDATA\\nvim <CR>")
+if package.config:sub(1, 1) == "\\" then
+  map("n", "<leader>cf", "<cmd> :cd $LOCALAPPDATA\\nvim <CR>")
 else
-    map("n", "<leader>cf", "<cmd> :cd ~/.config/nvim <CR>")
+  map("n", "<leader>cf", "<cmd> :cd ~/.config/nvim <CR>")
 end
 map("n", "<leader>tg", "<cmd> :Telescope repo list <CR>")
 -- map( {"n", "i", "v"}, "<RightClick>", "<cmd>lua require('menu').open('default')<cr>")
-map("n", "<RightMouse>", function ()
+map("n", "<RightMouse>", function()
   local options = vim.bo.ft == "NvimTree" and "nvimtree" or "default"
   require("menu").open(options, { mouse = true })
 end)
 
-map("n", "<leader>th", function ()
+map("n", "<leader>th", function()
   require("nvchad.themes").open { style = "bordered" }
 end)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
